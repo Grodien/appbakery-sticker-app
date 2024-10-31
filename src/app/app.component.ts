1bde4e1dc8f2ab9449e362a1020cd30e7d117fd8
kirill1722 <28723230+kirill1722@users.noreply.github.com>
2024-10-31 15:16:13 +0100

    esta style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+
+import '@sbb-esta/lyne-elements/button.js';
+import '@sbb-esta/lyne-elements/header.js';
+import '@sbb-esta/lyne-elements/menu.js';
+import '@sbb-esta/lyne-elements/navigation.js';
+import '@sbb-esta/lyne-elements/tag.js';
+import '@sbb-esta/lyne-elements/status.js';
+import '@sbb-esta/lyne-elements/container.js';
+import '@sbb-esta/lyne-elements/title.js';
+import '@sbb-esta/lyne-elements/form-field.js';
+import '@sbb-esta/lyne-elements/notification.js';
+import '@sbb-esta/lyne-elements/loading-indicator.js';
+import '@sbb-esta/lyne-elements/form-error.js';
+import '@sbb-esta/lyne-elements/toast.js';
+import '@sbb-esta/lyne-elements/datepicker.js';
+import '@sbb-esta/lyne-elements/accordion.js';
+import '@sbb-esta/lyne-elements/select.js';
+import '@sbb-esta/lyne-elements/option.js';
+import '@sbb-esta/lyne-elements/dialog.js';
 
 Amplify.configure(outputs);
 
@@ -11,7 +30,8 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
